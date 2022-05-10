39ebaaa8c55de1292cb83463f13fdf200e1c9194
unknown <Ataiborinbaev@gmail.com>
2022-05-10 19:08:04 +0600

    hws

---
+++ DZ1_Atai-14-01/greeting.js
@@ -0,0 +1,27 @@
+const name = prompt('Введите имя');
+alert("hello   " + name);
+
+const arr = [1221,3121,111];
+const arr2 = [122231,12231123,122121221,1111];
+    if (arr.length > arr2.length){
+        alert("arr больше чем arr2")
+    }else if (arr2.length > arr.length){
+        alert("arr2 больше чем arr")
+    }else{
+        alert("они равны")
+    }
+
+const color =prompt("выбери цвет:красный,желтый,зеленый")
+switch (color) {
+    case "красный":
+        alert('стой')
+        break;
+    case "желтый":
+        alert('готовимся')
+        break;
+    case "зеленый":
+        alert('БегиВасяБеги')
+        break;
+    default:
+       alert("Темирлан выбери зеленый")
+}
